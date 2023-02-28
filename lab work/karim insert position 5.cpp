a6841df29af93b19eece20b71e9f5b966302480a
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-02-28 19:57:11 +0600

    Create karim insert position 5.cpp

---
+++ lab work/karim insert position 5.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast() ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    fast();
+
+    string s[9] = { "mahfuz","prodhan","prodhan","prodhan","rahim","sahanur ","sujan"};
+    string add = "Karim";
+    int addpos = 5;
+    cout<<sizeof(s)/sizeof(s[0])<<endl;
+
+    // Shift elements to the right to create space for the new element
+    for(int i = 8; i > addpos; i--)
+    {
+        s[i] = s[i-1];
+    }
+
+    // Insert the new element at the desired position
+    s[addpos] = add;
+
+    // Print the updated array
+    for(int i = 0; i < 9; i++)
+    {
+        cout << s[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
